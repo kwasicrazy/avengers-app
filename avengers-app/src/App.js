945a7a2eedb9f5572a4d6f43b8e644caaf669bf8
Seth <sethtsatsu0@gmail.com>
2023-03-15 02:11:04 -0700

    react practice

---
+++ avengers-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.css';
+
+
+function App() {
+  return (
+    <div className="code-play">
+      <h1 style={{color:"red"}}>Codetrain is Awesome <span>!</span></h1> &nbsp
+      <h2 style={{color:"red"}}>My first react-app</h2>
+    </div>
+  );
+}
+
+export default App;
